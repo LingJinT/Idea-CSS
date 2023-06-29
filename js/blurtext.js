332a169e80ce5d8d4f694cbf83796a39b2d8dcaf
l2015 <lingjt@dtdream.com>
2023-06-29 12:14:48 +0800

    feat: 3d

--- js/blurtext.js
+++ js/blurtext.js
@@ -1,10 +1,10 @@
 function Blurtext() {
   return (
-    <div class="g-container">
-      <div class="word">这里有一些</div>
-      <div class="word">令人惊叹的</div>
-      <div class="word">CSS</div>
-      <div class="word">Let's Go</div>
+    <div className="g-container">
+      <div className="word">这里有一些</div>
+      <div className="word">令人惊叹的</div>
+      <div className="word">CSS</div>
+      <div className="word">Let's Go</div>
     </div>
   );
 }
---
+++ js/three.js
@@ -0,0 +1,20 @@
+function Three() {
+  return (
+    <div class="g-three">
+      <div class="g-group">
+        <div class="item item-right"></div>
+        <div class="item item-left"></div>
+        <div class="item item-top"></div>
+        <div class="item item-bottom"></div>
+        <div class="item item-middle"></div>
+      </div>
+      <div class="g-group">
+        <div class="item item-right"></div>
+        <div class="item item-left"></div>
+        <div class="item item-top"></div>
+        <div class="item item-bottom"></div>
+        <div class="item item-middle"></div>
+      </div>
+    </div>
+  );
+}
